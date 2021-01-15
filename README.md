3aed98ba605c7d8bd6d167360f6030d0e0bc72da
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2021-01-15 20:13:10 +0000

    added mini buttons - fixed mobile view - scrollbar on input - toggle checkbox css

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ https://8ball.delegator.com/
 
 https://greensock.com/docs/v3/Installation
 
+https://developer.mozilla.org/en-US/docs/Web/CSS/::-webkit-scrollbar
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_switch
+
 /_
 TODO: 1. Splash screen 2. Interactive button to start chat 3. Messenger style split screen 4. Local Storage to store messages 5. Options 6. API responses from 8ball (check for question marks)
 _/
--- scripts/components.js
+++ scripts/components.js
@@ -1,53 +1,60 @@
 // CREATE THE HOMEPAGE
 const createHomepage = () => {
-  let home = document.createElement("section");
-  $(home).attr("id", "home");
-  let leftPanel = document.createElement("section");
-  $(leftPanel).attr("class", "left-panel");
-  let rightPanel = document.createElement("section");
-  $(rightPanel).attr("class", "right-panel");
+  let home = document.createElement('section');
+  $(home).attr('id', 'home');
+  let leftPanel = document.createElement('section');
+  $(leftPanel).attr('class', 'left-panel');
+  let rightPanel = document.createElement('section');
+  $(rightPanel).attr('class', 'right-panel');
   $(home).append(leftPanel);
   $(home).append(rightPanel);
-  $("body").html(home);
+  $('body').html(home);
   return { home, leftPanel, rightPanel };
 };
 
 // CREATE THE LEFT PANEL
 function buildLeftPanel(leftPanel) {
-  let headerPanel = document.createElement("section");
-  $(headerPanel).attr("class", "left-header-panel");
-  let title = document.createElement("h2");
-  $(title).text("Rubber Duck");
+  let headerPanel = document.createElement('section');
+  $(headerPanel).attr('class', 'left-header-panel');
+  let title = document.createElement('h2');
+  $(title).text('Rubber Duck');
   // create 3 action buttons
-  let { qBtn, iBtn, sBtn } = buildOptions();
-  $(headerPanel).append(title, qBtn, iBtn, sBtn);
-  $(qBtn).click(() => questionButton(messagePanel));
-  $(iBtn).click(() => inspirationButton(messagePanel));
-  $(sBtn).click(() => successButton());
-  let messagePanel = document.createElement("section");
-  $(messagePanel).attr("class", "left-message-panel");
-  let footerPanel = document.createElement("section");
-  $(footerPanel).attr("class", "left-footer-panel");
+  let miniBtnContainer = document.createElement('div');
+  $(miniBtnContainer).attr('class', 'mini-btn-container');
+  let { qMiniBtn, iMiniBtn, sMiniBtn } = buildMiniOptions();
+  $(miniBtnContainer).append(qMiniBtn, iMiniBtn, sMiniBtn);
+  $(headerPanel).append(title, miniBtnContainer);
+  $(qMiniBtn).click(() => questionButton(messagePanel));
+  $(iMiniBtn).click(() => inspirationButton(messagePanel));
+  $(sMiniBtn).click(() => successButton());
+  let messagePanel = document.createElement('section');
+  $(messagePanel).attr('class', 'left-message-panel');
+  let footerPanel = document.createElement('section');
+  $(footerPanel).attr('class', 'left-footer-panel');
   $(leftPanel).append(headerPanel, messagePanel, footerPanel);
   // create the input element
-  let userInput = textArea("Type here", "user-input");
+  let userInput = textArea('Type here', 'user-input');
   // create send button
-  let sendButton = document.createElement("button");
-  $(sendButton).attr("class", "send-msg-btn");
+  let sendButton = document.createElement('button');
+  $(sendButton).attr('class', 'send-msg-btn');
+  let sendButtonIcon = document.createElement('img');
+  $(sendButtonIcon).attr('class', 'send-msg-btn-icon');
+  $(sendButtonIcon).attr('src', 'assets/images/send.png');
+  $(sendButton).append(sendButtonIcon);
   // append to footer panel
   $(footerPanel).append(userInput, sendButton);
 
   $(userInput).keydown(autosize);
   // create initial duck messages
   message(
-    "Yes, I am a duck 🦆 Yes, I am listening 👂 What’s up?",
-    "duck",
+    'Yes, I am a duck 🦆 Yes, I am listening 👂 What’s up?',
+    'duck',
     messagePanel
   );
   setTimeout(() => {
     message(
       "I will try to preducked the best times to respond, but if you would like to get some feedback, click the <span class='red'>red</span> button.\n\n If instead you would like something more inspirational, click the <span class='yellow'>yellow</span> button.\n\n When you arrive at a solution to your problem, congratulations! Choose the <span class='green'>green</span> button to complete your Rubber Ducky session!",
-      "duck",
+      'duck',
       messagePanel
     );
   }, 1000);
@@ -57,30 +64,34 @@ function buildLeftPanel(leftPanel) {
 // CREATE THE RIGHT PANEL
 function buildRightPanel(home, messagePanel, rightPanel) {
   // create darkmode toggle
-  let modeToggle = document.createElement("input");
-  $(modeToggle).attr("type", "checkbox");
-  $(modeToggle).attr("id", "mode");
-  // $(modeToggle).attr("value", "light");
+  const modeLabel = document.createElement('label');
+  $(modeLabel).attr('class', 'switch');
+  const modeSpan = document.createElement('span');
+  $(modeSpan).attr('class', 'slider round');
+  let modeToggle = document.createElement('input');
+  $(modeToggle).attr('type', 'checkbox');
+  $(modeToggle).attr('id', 'mode');
+  $(modeLabel).append(modeToggle, modeSpan);
   // create duck img element
-  let duckImg = document.createElement("img");
-  $(duckImg).attr("id", "duck-image");
-  $(duckImg).attr("src", "assets/images/rubber_duck.png");
-  $(duckImg).attr("alt", "Rubber Duck");
+  let duckImg = document.createElement('img');
+  $(duckImg).attr('id', 'duck-image');
+  $(duckImg).attr('src', 'assets/images/rubber_duck.png');
+  $(duckImg).attr('alt', 'Rubber Duck');
   // create button container
-  let btnContainer = document.createElement("section");
-  $(btnContainer).attr("class", "btn-container");
-  $(rightPanel).append(modeToggle, duckImg, btnContainer);
+  let btnContainer = document.createElement('section');
+  $(btnContainer).attr('class', 'btn-container');
+  $(rightPanel).append(modeLabel, duckImg, btnContainer);
   let { qBtn, iBtn, sBtn } = buildOptions();
   $(btnContainer).append(qBtn, iBtn, sBtn);
 
   $(modeToggle).change(function () {
     if (this.checked) {
-      console.log("Checked");
-      $(rightPanel).addClass("darkmode");
-      $(messagePanel).addClass("darkmode");
+      console.log('Checked');
+      $(rightPanel).addClass('darkmode');
+      $(messagePanel).addClass('darkmode');
     } else {
-      $(rightPanel).removeClass("darkmode");
-      $(messagePanel).removeClass("darkmode");
+      $(rightPanel).removeClass('darkmode');
+      $(messagePanel).removeClass('darkmode');
     }
   });
 
@@ -92,22 +103,37 @@ function buildRightPanel(home, messagePanel, rightPanel) {
 // CREATE OPTION BUTTONS
 function buildOptions() {
   // create three buttons
-  let qBtn = document.createElement("button");
-  $(qBtn).attr("class", "duck-btn");
-  $(qBtn).attr("id", "question-btn");
-  let iBtn = document.createElement("button");
-  $(iBtn).attr("class", "duck-btn");
-  $(iBtn).attr("id", "inspiration-btn");
-  let sBtn = document.createElement("button");
-  $(sBtn).attr("class", "duck-btn");
-  $(sBtn).attr("id", "success-btn");
+  let qBtn = document.createElement('button');
+  $(qBtn).attr('class', 'duck-btn');
+  $(qBtn).attr('id', 'question-btn');
+  let iBtn = document.createElement('button');
+  $(iBtn).attr('class', 'duck-btn');
+  $(iBtn).attr('id', 'inspiration-btn');
+  let sBtn = document.createElement('button');
+  $(sBtn).attr('class', 'duck-btn');
+  $(sBtn).attr('id', 'success-btn');
   return { qBtn, iBtn, sBtn };
+}
+
+// CREATE MINI OPTION BUTTONS
+function buildMiniOptions() {
+  // create three buttons
+  let qMiniBtn = document.createElement('button');
+  $(qMiniBtn).attr('class', 'duck-mini-btn');
+  $(qMiniBtn).attr('id', 'question-mini-btn');
+  let iMiniBtn = document.createElement('button');
+  $(iMiniBtn).attr('class', 'duck-mini-btn');
+  $(iMiniBtn).attr('id', 'inspiration-mini-btn');
+  let sMiniBtn = document.createElement('button');
+  $(sMiniBtn).attr('class', 'duck-mini-btn');
+  $(sMiniBtn).attr('id', 'success-mini-btn');
+  return { qMiniBtn, iMiniBtn, sMiniBtn };
 }
 
 // CREATE A MESSAGE BOX
 function message(text, type, container) {
-  const msg = document.createElement("div");
-  $(msg).attr("class", `${type}-message`);
+  const msg = document.createElement('div');
+  $(msg).attr('class', `${type}-message`);
   $(msg).html(text);
   $(container).append(msg);
   return msg;
@@ -115,20 +141,20 @@ function message(text, type, container) {
 
 // CREATE THE INPUT
 function textArea(placeholder, id) {
-  const ip = document.createElement("textarea");
-  $(ip).attr("class", "input");
-  $(ip).attr("id", id);
-  $(ip).attr("placeholder", placeholder);
-  $(ip).attr("cols", "50");
-  $(ip).attr("rows", "1");
+  const ip = document.createElement('textarea');
+  $(ip).attr('class', 'input');
+  $(ip).attr('id', id);
+  $(ip).attr('placeholder', placeholder);
+  $(ip).attr('cols', '50');
+  $(ip).attr('rows', '1');
   return ip;
 }
 
 function buildSuccessModal(home) {
-  const bg = document.createElement("div");
-  $(bg).attr("class", "success-modal-container");
-  const sModal = document.createElement("section");
-  $(sModal).attr("class", "success-modal");
+  const bg = document.createElement('div');
+  $(bg).attr('class', 'success-modal-container');
+  const sModal = document.createElement('section');
+  $(sModal).attr('class', 'success-modal');
   $(bg).append(sModal);
   $(home).append(bg);
 }
@@ -136,12 +162,12 @@ function buildSuccessModal(home) {
 function autosize() {
   var el = this;
   if (el.scrollHeight > 120) {
-    el.style.cssText = "overflow: scroll !important; height: 120px";
-    el.removeEventListener("keydown", autosize);
+    el.style.cssText = 'overflow-y: scroll !important; height: 120px';
+    el.removeEventListener('keydown', autosize);
   } else {
     setTimeout(function () {
-      el.style.cssText = "height:auto; padding:0";
-      el.style.cssText = "height:" + el.scrollHeight + "px";
+      el.style.cssText = 'height:auto; padding:0';
+      el.style.cssText = 'height:' + el.scrollHeight + 'px';
     }, 0);
   }
 }
