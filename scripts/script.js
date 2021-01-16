c1fb0fa5996a4fd1761ece26c58a0be1350009fb
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2021-01-16 09:53:14 +0000

    update localStorage with chatLogs

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
   //  SPLASH SCREEN
+  localStorage.setItem('chat', JSON.stringify([]));
   let percentage = 0;
   let timer = setInterval(() => {
     percentage++;
-    $(".loading-bar").css("width", `${percentage}%`);
+    $('.loading-bar').css('width', `${percentage}%`);
     if (percentage === 100) {
-      $(".start-button").attr("disabled", false).removeClass("disabled-btn");
+      $('.start-button').attr('disabled', false).removeClass('disabled-btn');
       clearInterval(timer);
     }
   }, 1);
 
   // PRESS START
-  $(".start-button").on("click", () => {
+  $('.start-button').on('click', () => {
     // CLEAR THE PAGE
-    removeElemByClass(["heading", "loading-container", "start-button"]);
+    removeElemByClass(['heading', 'loading-container', 'start-button']);
     // RUN FUNCTION TO CREATE HOMEPAGE ELEMENTS
     let { home, leftPanel, rightPanel } = createHomepage();
     // BUILD LEFT PANEL
@@ -23,24 +24,27 @@ $(document).ready(function () {
 
     // USER SENDS A MESSAGE
     $(sendButton).click(() => {
-      console.log("Send message button was clicked!");
+      console.log('Send message button was clicked!');
       let userMsg = $(userInput).val();
-      if (userMsg === "") return;
+      if (userMsg === '') return;
       else {
+        // add to local storage
+        updateChatHistory(userMsg, true);
         // check message for errors here
-        message(userMsg, "user", messagePanel);
+        message(userMsg, 'user', messagePanel);
         // reset the input
-        $(userInput).val("");
+        $(userInput).val('');
       }
       // check if users message ended with question mark
-      if (userMsg.charAt(userMsg.length - 1) === "?") {
-        console.log("User asked a question");
+      if (userMsg.charAt(userMsg.length - 1) === '?') {
+        console.log('User asked a question');
         setTimeout(() => {
           // choose a randome duck response
           const response = Math.floor(
             Math.random() * duckQuestionResponses.length
           );
-          message(duckQuestionResponses[response], "duck", messagePanel);
+          updateChatHistory(duckQuestionResponses[response], false);
+          message(duckQuestionResponses[response], 'duck', messagePanel);
         }, 1000);
       }
     });
--- scripts/utils.js
+++ scripts/utils.js
@@ -9,42 +9,61 @@ function removeElemById(args) {
 const duckQuestionResponses = [
   "I can see you asked me a question, I'd love to help, but I am a duck...",
   "Wait... you're seriously asking me? ... you do realise I'm a duck?",
-  "Well I mean, I guess, the question is what do YOU think about it?",
+  'Well I mean, I guess, the question is what do YOU think about it?',
   "Interesting question, I know a guy who might be able to help you with that...he's YOU!",
   "That's a tough one for me to answer, maybe try to rephrase it for me?",
   "It's like my father used to say, 'Quack quack quack qua-' oh wait, you don't speak duck do you? Well tough, I'm not repeating myself...",
 ];
 
 async function questionButton(messagePanel) {
-  console.log("Question Button Clicked!");
+  console.log('Question Button Clicked!');
   // make call to 8-ball API
   try {
     const { magic } = await eightBallAPI();
     console.log(magic.answer);
-    message(magic.answer, "duck", messagePanel);
+    updateChatHistory(magic.answer, false);
+    message(magic.answer, 'duck', messagePanel);
   } catch (err) {
-    message("Messed up...", "duck", messagePanel);
+    updateChatHistory('Messed up...', false);
+    message('Messed up...', 'duck', messagePanel);
   }
 }
 
 async function inspirationButton(messagePanel) {
-  console.log("Inspiration Button Clicked!");
+  console.log('Inspiration Button Clicked!');
   try {
     // make API call to inspirational Quotes
     const { contents } = await getQuote();
     const quote = contents.quotes[0].quote;
-    message(quote, "duck", messagePanel);
+    updateChatHistory(quote, false);
+    message(quote, 'duck', messagePanel);
   } catch (err) {
+    updateChatHistory(
+      "I'm all out of quotes for now, you'll have to wait until I can think of more...",
+      false
+    );
     message(
       "I'm all out of quotes for now, you'll have to wait until I can think of more...",
-      "duck",
+      'duck',
       messagePanel
     );
   }
 }
 
 function successButton() {
-  console.log("Success Button Clicked!");
+  console.log('Success Button Clicked!');
   // create modal window
   $();
+}
+
+function updateChatHistory(msg, userType) {
+  let chatHistory = JSON.parse(localStorage.getItem('chat'));
+
+  var chatMessage = {
+    name: userType ? 'user' : 'duck',
+    text: msg,
+    createdAt: new Date(),
+  };
+  chatHistory.push(chatMessage);
+  localStorage.setItem('chat', JSON.stringify(chatHistory));
 }
